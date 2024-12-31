cb3e35369ba39a036c1dbe4ea1edffcd3c900b81
Veselin Ivanov <me@veselyn.com>
2024-12-31 10:41:52 +0000

    feat: configure file logger

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@ mod cli;
 mod container;
 mod state;
 
+use std::env::temp_dir;
+
 use clap::Parser;
 use cli::Cli;
 use log::debug;
 
 fn main() -> anyhow::Result<()> {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .init();
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!("[{}] {}", record.level(), message));
+        })
+        .chain(std::io::stderr())
+        .chain(fern::log_file(temp_dir().join("containr.log"))?)
+        .apply()?;
 
-    debug!(args:? = std::env::args(); "received cli args");
+    debug!("received cli args {:?}", std::env::args());
 
     let cli = Cli::parse();
     cli.run()
